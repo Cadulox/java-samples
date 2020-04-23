1389e4437d87b8a00006543a2891263b9ebeb5d0
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-04-23 14:58:47 -0300

    Examples

---
+++ Comportamento de memória, arrays, lists/BoxingUnboxing/src/application/Program.java
@@ -0,0 +1,20 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		int x = 20;
+		
+		//Object obj = x;
+		Integer obj = x; // Wrapper classes
+		
+		System.out.println(obj);
+		
+		//int y = (int) obj;
+		int y = obj * 2;
+		
+		System.out.println(y);
+	}
+
+}
