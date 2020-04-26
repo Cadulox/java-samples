ea6df1383077d836b87fa20fb77251a6802bdbcf
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-04-26 20:34:43 -0300

    Examples

---
+++ Herança e polimorfismo/SolvedExercisePolymorphism/src/entities/OutsorcedEmployee.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class OutsorcedEmployee extends Employee {
+
+	private double additionalCharge;
+	
+	public OutsorcedEmployee() {
+		
+	}
+
+	public OutsorcedEmployee(String name, Integer hours, Double valuePerHour, double additionalCharge) {
+		super(name, hours, valuePerHour);
+		this.additionalCharge = additionalCharge;
+	}
+
+	public double getAdditionalCharge() {
+		return additionalCharge;
+	}
+
+	public void setAdditionalCharge(double additionalCharge) {
+		this.additionalCharge = additionalCharge;
+	}
+	
+	@Override
+	public double payment() {
+		return super.payment() + additionalCharge * 1.1;
+	}
+}
